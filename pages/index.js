b3f57871eea599a9849860c7255bbc355a821f07
Freddy Vegas <vegasfalexis@gmail.com>
2023-09-18 11:26:15 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+   <div className={'bg-blue-900 w-screen h-screen flex'}>
+    <div>
+      <button className="bg-white p-2">Login with Google</button> 
+    </div>
+    </div>
+  )
+}
