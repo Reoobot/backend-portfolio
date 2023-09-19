c13b70df50f7b522a308a273061f9b5d573c17b8
Freddy Vegas <vegasfalexis@gmail.com>
2023-09-19 10:42:58 +0200

    inactive of nav

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="bg-blue-900 min-h-screen flex">
       <Nav/>
-      <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4">logged in {session.user.email}</div>
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">logged in {session.user.email}</div>
     </div>
   )
 }
